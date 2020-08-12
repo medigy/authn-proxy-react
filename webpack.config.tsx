b0d0d121a5f90fc6001566e94b45da385715c529
UnniPanicker <unni.krishnan@citrusinformatics.com>
2020-08-12 17:15:28 +0530

    modify endpoint

--- webpack.config.tsx
+++ webpack.config.tsx
@@ -1,9 +1,6 @@
 export const Config = {
-  //postgraphileUrl:"http://localhost:8084/http://sandbox.postgraphile.appx.docker.localhost/graphql",
-  postgraphileUrl:
-    "http://localhost:8084/https://dev-pggql.unblock.health/graphql", //with token pass
   authUrl:
-    "http://localhost:8084/https://devl.gl.infra.unblock.health/oauth/token",
+    "https://devl.gl.infra.unblock.health/oauth/token",
   scope: "openid api profile email",
   clientId: "d99f26bb232a9ea6f2025dc759682326b6dc70cedbcac9bd50eb09d87c70e712",
 };
